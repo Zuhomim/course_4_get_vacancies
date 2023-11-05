03880fc435f32797f0c2892c3ffcfd64569aa255
Dmitri Sementsov <pluribus.zuhi@gmail.com>
2023-11-05 17:12:18 +0700

    add method read_json in json_saver

--- src/json_saver.py
+++ src/json_saver.py
@@ -1,7 +1,10 @@
 import json
 
+from src.vacancy import Vacancy
+
 
 class JSONSaver:
+    """Класс записи и получения данных в директории json"""
 
     def __init__(self, filename):
         self.__filename = filename
@@ -11,5 +14,27 @@ class JSONSaver:
         return self.__filename
 
     def write_json(self, data):
+        """Метод записи файла в директорию json"""
+
         with open(f"../json/{self.filename}.json", 'w', encoding="utf-8") as f:
             json.dump(data, f, indent=2, ensure_ascii=False)
+
+    def read_json(self):
+        """Метод получения данных из файла в директории json"""
+
+        with open(f"../json/{self.filename}.json", 'rt', encoding="utf-8") as f:
+            vacancies = json.load(f)
+
+        all_vacancies = []
+
+        for vacancy in vacancies:
+            all_vacancies.append(Vacancy(
+                vacancy["name"],
+                vacancy["description"],
+                vacancy["company_name"],
+                vacancy["salary_from"],
+                vacancy["salary_to"],
+                vacancy["url"]
+            ))
+
+        return all_vacancies
