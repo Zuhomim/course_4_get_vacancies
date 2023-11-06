1097a103a1f96f3a5c7e470aa82c2a9917424fac
Dmitri Sementsov <pluribus.zuhi@gmail.com>
2023-11-07 00:58:45 +0700

    fixed vacancy

--- src/vacancy.py
+++ src/vacancy.py
@@ -14,9 +14,9 @@ class Vacancy:
                 f'Salary: {self.salary_from}, URL: {self.url})')
 
     def __str__(self):
-        return f'Название: {self.name}\nОписание: {self.description}\nЗарплата: {self.salary_from}'
+        return f'Название: {self.name}\nОписание: {self.description}\nЗарплата: {self.salary_from}\nСсылка: {self.url}'
 
-    def __init__(self, name, description, company_name, salary_from, salary_to, url):
+    def __init__(self, name: str, description: str, company_name: str, salary_from: int, salary_to: int, url: str):
         self.name = name
         self.description = description
         self.company_name = company_name
@@ -25,7 +25,25 @@ class Vacancy:
         self.url = url
 
     def __lt__(self, other):
+        """Метод сравнения для сортировки списка вакансий"""
+
         return self.salary_from < other.salary_from
 
     def __gt__(self, other):
+        """Метод сравнения для сортировки списка вакансий"""
+
         return self.salary_from > other.salary_from
+
+    def class_to_dict(self):
+        """Метод подготовки свойств класса для записи в json-файл"""
+
+        fields = {
+            "name": self.name,
+            "description": self.description,
+            "company_name": self.company_name,
+            "salary_from": self.salary_from,
+            "salary_to": self.salary_to,
+            "url": self.url
+        }
+
+        return fields
