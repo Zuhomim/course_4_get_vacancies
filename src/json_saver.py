60a67e2efaaf9989d622d25f5c668e349443d418
Dmitri Sementsov <pluribus.zuhi@gmail.com>
2023-11-07 00:56:27 +0700

    add methods for json_saver

--- src/json_saver.py
+++ src/json_saver.py
@@ -1,40 +1,99 @@
 import json
-
+import os
 from src.vacancy import Vacancy
 
 
 class JSONSaver:
     """Класс записи и получения данных в директории json"""
 
-    def __init__(self, filename):
-        self.__filename = filename
-
-    @property
-    def filename(self):
-        return self.__filename
-
-    def write_json(self, data):
+    @staticmethod
+    def write_json(data):
         """Метод записи файла в директорию json"""
 
-        with open(f"../json/{self.filename}.json", 'w', encoding="utf-8") as f:
+        with open(f"../json/vacancies.json", 'w', encoding="utf-8") as f:
             json.dump(data, f, indent=2, ensure_ascii=False)
 
-    def read_json(self):
+    @staticmethod
+    def read_json():
         """Метод получения данных из файла в директории json"""
 
-        with open(f"../json/{self.filename}.json", 'rt', encoding="utf-8") as f:
+        with open(f"../json/vacancies.json", 'rt', encoding="utf-8") as f:
             vacancies = json.load(f)
 
+        return vacancies
+
+    @staticmethod
+    def get_vacancies():
+        """Метод получения списка вакансий"""
+
         all_vacancies = []
+        vacancies = JSONSaver.read_json()
+        for vacancy_item in vacancies:
+            all_vacancies.append(Vacancy(
+                vacancy_item["name"],
+                vacancy_item["description"],
+                vacancy_item["company_name"],
+                vacancy_item["salary_from"],
+                vacancy_item["salary_to"],
+                vacancy_item["url"]
+            ))
+
+        return all_vacancies
+
+    def add_vacancy(self, vacancy: Vacancy):
+        """Метод добавления вакансии в список вакансий"""
+
+        all_vacancies = []
+        vacancy_to_json = {
+                    "name": vacancy.name if vacancy.name else "",
+                    "description": vacancy.description if vacancy.description else "",
+                    "company_name": vacancy.company_name if vacancy.company_name else "",
+                    "salary_from": vacancy.salary_from if vacancy.salary_from else 0,
+                    "salary_to": vacancy.salary_to if vacancy.salary_to else 0,
+                    "url": vacancy.url if vacancy.url else ""
+                }
+
+        if os.path.exists("../json/vacancies.json"):
+            all_vacancies = self.read_json()
+            all_vacancies.append(vacancy_to_json)
+        else:
+            all_vacancies.append(vacancy_to_json)
+            self.write_json(vacancy_to_json)
+
+        self.write_json(all_vacancies)
+
+    def delete_vacancy(self, vacancy_to_del: Vacancy):
+        """Метод удаления вакансии из списка вакансий"""
+
+        all_vacancies = self.get_vacancies()
+
+        data = [vacancy.class_to_dict() for vacancy in all_vacancies if (
+                    vacancy.name != vacancy_to_del.name
+                    or vacancy.company_name != vacancy_to_del.company_name
+                    or vacancy.salary_from != vacancy_to_del.salary_from
+        )]
+
+        self.write_json(data)
+
+    def get_vacancies_by_salary(self, salary_range: str):
+        """Метод вывода вакансий по желаемой зарплате / диапазону зарплат"""
+
+        all_vacancies = []
+        salary_to = 0
+        vacancies = self.get_vacancies()
+
+        if "-" in salary_range:
+            salary_from = int(salary_range.split("-")[0].replace(" ", ""))
+            salary_to = int(salary_range.split("-")[1].replace(" ", ""))
+        else:
+            salary_from = int(salary_range.replace(" ", ""))
 
         for vacancy in vacancies:
-            all_vacancies.append(Vacancy(
-                vacancy["name"],
-                vacancy["description"],
-                vacancy["company_name"],
-                vacancy["salary_from"],
-                vacancy["salary_to"],
-                vacancy["url"]
-            ))
+            if salary_to > 0:
+                if salary_from <= vacancy.salary_from <= salary_to:
+                    all_vacancies.append(vacancy)
+            else:
+                if salary_from <= vacancy.salary_from:
+                    all_vacancies.append(vacancy)
 
         return all_vacancies
